0010ae12f53b600781838329fd485afa9fbc957f
thameemk612 <thameemk612@gmail.com>
2019-03-11 18:29:54 +0530

    sponsor

---
+++ sponsors/index.php
@@ -0,0 +1,94 @@
+<html>
+<head>
+  <title>Hestia19</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+  <link rel="icon" type="image/png" href="../assets/img/hestia-icon.png">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <link rel="stylesheet" href="../assets/css/eventium-assets.min.css">
+  <link rel="stylesheet" href="../assets/css/style-sponsor.css">
+  <link href="assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
+  <link href="../assets/fonts/Hestia.css?family=Hestia-Regular" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+
+  <style>
+  body{
+    background: url('../assets/img/about_us_bg.jpg') no-repeat center center fixed;
+    -webkit-background-size: cover;
+    -moz-background-size: cover;
+    -o-background-size: cover;
+    background-size: cover;
+  }
+  .sponsor{
+    overflow:none;
+  }
+  @media screen and (min-width: 767px) {
+    .sponsor {
+      overflow: hidden;
+    }
+  }
+  .listing4 {
+
+    font-family: 'Hestia-Regular', sans-serif !important;
+    color: white;
+  }
+  .back_btn{
+
+left:10px;
+}
+
+
+  @media screen and (min-width: 767px) {
+
+    .back_btn{
+      left:40px;
+    }
+  }
+  .spo_box{
+    /* background-color:white; */
+    /* margin:5px; */
+    padding: 5px;
+
+  }
+  .spo_box a{
+    margin:auto;
+    width:100%;
+    height:110px;
+      background-color:white;
+
+  }
+  .spo_box a img{
+    /* max-height: 110px; */
+    height:110px;
+    max-width: 110
+
+
+  }
+
+  </style>
+</head>
+<body class="sponsor ">
+<section class="clients-section">
+			<div class="container text-center">
+
+          <h1 class="listing4 "><a  class="back_btn" style="position:absolute;text-decoration:none;color:white;" href="#" onclick="window.history.back();"><i class="fa fa-arrow-left" aria-hidden="true"></i>
+    </a> SPONSORS</h1>
+					<p class="listing4">Praesent elementum hendrerit tortor. Sed semper lorem at felis. Vestibulum volutpat, lacus a ultrices sagittis, mi neque euismod dui, eu pulvinar nunc sapien ornare nisl. Phasellus pede arcu, dapibus eu, fermentum et, dapibus sed, urna.</p>
+
+          <div class="client-box ">
+            <div class=" row client-list">
+              <div class="col-md-3 col-sm-12 spo_box mx-auto"><a href="#"><img src="http://nunforest.com/eventium/images/clients/1.png" alt=""></a></div>
+              <div class="col-md-3 col-sm-12 spo_box"><a href="#"><img src="http://nunforest.com/eventium/images/clients/2.png" alt=""></a></div>
+              <div class="col-md-3 col-sm-12 spo_box"><a href="#"><img src="http://nunforest.com/eventium/images/clients/3.png" alt=""></a></div>
+              <div class="col-md-3 col-sm-12 spo_box"><a href="#"><img src="http://nunforest.com/eventium/images/clients/4.png" alt=""></a></div>
+            </div>
+            <div class=" row client-list">
+
+              <div class="spo_box"><a href="#"><img src="http://nunforest.com/eventium/images/clients/5.png" alt=""></a></div>
+              <div class="spo_box"><a href="#"><img src="http://nunforest.com/eventium/images/clients/6.png" alt=""></a></div>
+              <div class="spo_box"><a href="#"><img src="http://nunforest.com/eventium/images/clients/7.png" alt=""></a></div>
+              <div class="spo_box"><a href="#">Become a<br> sponsor</a></div>
+            </div>
+				</div>
+			</div>
+</section>
+</body>
