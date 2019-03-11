14353fccde5107db75bcf62a67292afeb45ca74b
thameemk612 <thameemk612@gmail.com>
2019-03-11 16:22:36 +0530

    loader image

--- assets/loader/loader.js
+++ assets/loader/loader.js
@@ -1,7 +1,7 @@
 $(window).on('load', function() { // makes sure the whole site is loaded
   $('#status').fadeOut(); // will first fade out the loading animation
-  $('#preloader').delay(1350).fadeOut('slow'); // will fade out the white DIV that covers the website.
-  $('body').delay(1350).css({
+  $('#preloader').delay(350).fadeOut('slow'); // will fade out the white DIV that covers the website.
+  $('body').delay(350).css({
     'overflow': 'visible'
   });
   $('#preloader').fadeOut('slow', function() {
