022d9cb1bf5412b598577639efdace4cfbc7e4b0
thameemk612 <thameemk612@gmail.com>
2019-03-11 10:49:54 +0530

    PHP & optimization

---
+++ backup/eventlist.php
@@ -0,0 +1,198 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/logo.jpg">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <title>
+  Hestia'19
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link href="assets/fonts/Hestia.css?family=Hestia-Regular" rel="stylesheet">
+  <link href="assets/fonts/Galgo.css?family=Galgo" rel="stylesheet">
+
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
+  <!-- CSS Just for demo purpose, don't include it in your project -->
+  <link href="assets/demo/demo.css" rel="stylesheet" />
+  <link rel="stylesheet" href="assets/css/main_style.css">
+  <style>
+  .navbar .navbar-nav .nav-item .nav-link{
+    font-size: 1.5rem;
+  }
+
+  *{
+    font-family: 'Hestia-Regular', sans-serif;
+  }
+  .listing1{
+      word-wrap:break-word;
+      word-break: break-all;
+      background-size: cover;
+      border-bottom: 0.5px solid gray;
+      padding-top:25px;
+      margin-bottom: 5vh;
+      font-family: 'Hestia-Regular', sans-serif;
+  }
+
+
+  .listing1 .event-name{
+
+      font-size: 2rem;
+      font-family: 'Hestia-Regular', sans-serif !important; 
+      color:white;
+  
+  }
+  .event-desc{
+      font-size: 1rem;
+      color: white;
+      margin-top: 3vh;
+      margin-bottom: 4vh;
+     
+    
+  }
+  .details{
+   
+      color: black;
+      background-color: azure;
+      text-decoration: none;
+      padding-left: 20px;
+      padding-right: 20px;
+      padding-top: 10px;
+      padding-bottom: 10px;
+      border-radius: 2px; 
+      margin-bottom: 4vh;
+     
+  }
+  .details:hover{
+      color:gray;;
+  }
+  .event_listing_div{
+margin-left:5vh;
+margin-right:5vh;
+margin-top:20px;
+}
+  @media (min-width: 786px) {
+.event_listing_div{
+  padding-left:10vh;
+padding-right:10vh;
+  margin-top:100px;
+  
+}
+  }
+</style>
+</head>
+
+<body class="profile-page sidebar-collapse page-header" data-parallax="true" style="background: url('assets/img/bg_tree.jpg') no-repeat center center fixed; -webkit-background-size: cover;-moz-background-size: cover;-o-background-size: cover;background-size: cover;height: 100%;overflow: scroll;">
+  <nav class="navbar navbar-transparent navbar-color-on-scroll fixed-top navbar-expand-lg" color-on-scroll="black" id="sectionsNav">
+    <div class="container-fluid">
+      <div class="navbar-translate">
+       <a href="./">
+          <img style="max-height:40px;" class="mobile-show" src="assets/img/logo-inline-with-text.png">
+        </a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="sr-only" style="color: black;">Toggle navigation</span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        
+      </div>
+      <div class="collapse navbar-collapse">
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            &nbsp;&nbsp;
+            &nbsp;&nbsp;
+            &nbsp;&nbsp;
+            &nbsp;&nbsp;
+            &nbsp;&nbsp;
+            &nbsp;&nbsp;
+          </li>
+        </ul>
+        <ul class="navbar-nav mx-auto">
+          <li class="nav-item fade-in">
+            <a href="#" class="nav-link event-click " id="events">
+              EVENTS
+            </a>
+          </li>
+          <li class="nav-item fade-in">
+            <a href="#" class="nav-link">
+              SPONSORS
+            </a>
+          </li>
+          <li class="nav-item d-none d-lg-block">
+            <a class="navbar-brand my-auto" href="./">
+              <img class="fade-in-top" style="max-height: 75px;margin-top: -20px;" src="assets/img/logo.png" /></a>
+          </li>
+          <li class="nav-item fade-in">
+            <a href="#" class="nav-link">
+              ABOUT
+            </a>
+          </li>
+          <li class="nav-item fade-in">
+            <a href="#" class="nav-link">
+              CONTACT
+            </a>
+          </li>
+        </ul>
+
+        <ul class="navbar-nav">
+          <li class="nav-item ">
+            <a href="#" class="nav-link btn btn-outline-primary float-left " style="border-color:white; margin-left: 12%;">
+              LOGIN
+            </a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class='clearfix'></div>
+<br>
+<div class="row event_listing_div"  style="text-align:left;padding-top: 10vh;"> 
+    <div class="col-12 listing1" style="padding-bottom: 20px;">
+              <p class="event-name text-light" style="letter-spacing: 3px;color:white;text-align: left;">CRANKWOJOYRIDE</p>
+              <p class="event-desc">Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.</p>
+              <a href="#" class="details">MORE&nbsp;DETAILS</a>
+            </div>
+    <div class="col-12 listing1" style="padding-bottom: 20px;">
+      <p class="event-name text-light" style="letter-spacing: 3px;text-align: left;">CRANKWOJOYRIDE</p>
+      <p class="event-desc">Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.</p>
+      <a href="#" class="details">MORE&nbsp;DETAILS</a>
+    </div>
+    <div class="col-12 listing1" style="padding-bottom: 20px;">
+      <p class="event-name text-light" style="letter-spacing: 3px;color:white;text-align: left;">CRANKWOJOYRIDE</p>
+      <p class="event-desc">Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.</p>
+      <a href="#" class="details">MORE&nbsp;DETAILS</a>
+    </div>
+<div class="col-12 listing1" style="padding-bottom: 20px;">
+<p class="event-name text-light" style="letter-spacing: 3px;text-align: left;">CRANKWOJOYRIDE</p>
+<p class="event-desc">Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.Lorem ipsum dolor sit amet, oratio homero gubergren in vel. Maiorum nominati te eos, et assum gloriatur omittantur qui. Probo aliquam intellegebat ex vix, paulo epicurei pro te. Stet consequuntur ad nec, oportere democritum sit ex.</p>
+<a href="#" class="details">MORE&nbsp;DETAILS</a>
+</div>
+  </div>
+
+
+  <!--   Core JS Files   -->
+  <script src="assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="assets/js/plugins/moment.min.js"></script>
+  <!--  Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
+  <script src="assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
+  <script src="assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
+  <script src="assets/js/material-kit.js?v=2.0.5" type="text/javascript"></script>
+  <script type="text/javascript">
+    $("#carousel").hide();
+    $(".event-click").click(function(){
+      $("#carousel").show();
+    });
+  </script>
+</body>
+
+</html>
---
+++ contact/index.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <link rel="apple-touch-icon" sizes="76x76" href="assets/img/apple-icon.png">
+  <link rel="icon" type="image/png" href="assets/img/logo.jpg">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
+  <meta name="theme-color" content="#1a3840">
+  <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css" integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+  <title>
+    Hestia'19
+  </title>
+  <meta content='width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=0, shrink-to-fit=no' name='viewport' />
+  <!--     Fonts and icons     -->
+  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
+  <link href="../assets/fonts/Hestia.css?family=Hestia-Regular" rel="stylesheet">
+  <link rel="stylesheet" href="../assets/css/font-awesome.min.css">
+  <!-- CSS Files -->
+  <link href="../assets/css/material-kit.css?v=2.0.5" rel="stylesheet" />
+
+  <link rel="stylesheet" href="../assets/carousel/assets/owl.carousel.min.css">
+  <link rel="stylesheet" href="../assets/carousel/assets/owl.theme.default.min.css">
+  <link rel="stylesheet" href="../assets/css/custom.css">
+  <link rel="stylesheet" href="../assets/css/main_style.css">
+  <style>
+    body {
+      background: url('../assets/img/ediyt.jpg') no-repeat center center fixed;
+      -webkit-background-size: cover;
+      -moz-background-size: cover;
+      -o-background-size: cover;
+      background-size: cover;
+      overflow: scroll;
+    }
+  </style>
+</head>
+
+<body style="background-image:url('../assets/img/conjura.jpg')">
+  <div class="container  text-white">
+    <h2 class="text-center" style="font-weight: 700; margin-top: 15%; letter-spacing: 2px; font-family: 'Hestia-Regular', sans-serif;">CONTACT US</h2>
+    <div class="row mb-5">
+      <div class="col-md-2 col-sm-12">
+
+      </div>
+      <div class="col-md-2 col-sm-12">
+              <p style="font-weight: 700; font-size: 1.2em; text-align: center; ">PAUSHAL P A</p>
+              <p class="text-center" style=" letter-spacing: 1px;  text-align: center; ">
+              Arts Club Secretary<br>
+              College Union<br>
+              8891505295<br>
+              paushal@hestia.live<br>
+          </p>
+      </div>
+      <div class="col-md-2 col-sm-12 ">
+        <p style="font-weight: 700; font-size: 1.2em; text-align: center; ">NICKSON BABY V M</p>
+              <p class="text-center" style=" letter-spacing: 1px;  text-align: center; ">
+              Marketing Head<br>
+              HESTIA'19<br>
+              96454320167<br>
+              nickson@hestia.live<br>
+          </p>
+      </div>
+      <div class="col-md-2 col-sm-12 mx-auto">
+            <p style="font-weight: 700; font-size: 1.2em; text-align: center; ">ASHWIN P S</p>
+            <p class="text-center" style=" letter-spacing: 1px;  text-align: center; ">
+              General Secretary<br>
+              College Union<br>
+              9645198034<br>
+              ashwinps@hestia.live<br>
+          </p>
+      </div>
+      <div class="col-md-3 col-sm-12">
+          <p style="font-weight: 700; font-size: 1.2em; text-align: center; ">MOHEMED ANSER PM</p>
+          <p class="text-center" style=" letter-spacing: 1px;  text-align: center; ">
+              Chairman<br>
+              College Union<br>
+              8129715596<br>
+              anser@hestia.live<br>
+          </p>
+      </div>
+      <div class="col-md-1 col-sm-12">
+
+      </div>
+    </div>
+    <div class="row mb-4">
+      <div class="col-md-4 col-sm-12"></div>
+      <div class="col-md-4 col-sm-12" style=" letter-spacing: 2px;">
+        <p style="font-size: 2rem; text-align: center;"><i class="fab fa-instagram"></i>&nbsp;hestia.19</p>
+        <p style="font-size: 1.5rem; text-align: center;"><i class="far fa-envelope"></i></i>&nbsp;info@hestia.live</p>
+      </div>
+      <div class="col-md-4 col-sm-12"></div>
+    </div>
+    <div class="row">
+        <div class="col-md-4 col-sm-12"></div>
+        <div class="col-md-4 col-sm-12" style=" letter-spacing: 2px;">
+          <p style="font-weight: 700; font-size: 1.5em; text-align: center;">www.hestia.live</p>
+        </div>
+        <div class="col-md-4 col-sm-12"></div>
+      </div>
+  </div>
+
+</body>
+
+</html>
